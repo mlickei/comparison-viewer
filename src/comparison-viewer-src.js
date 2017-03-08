35ca09f43fd5efb928b51f2796a69dfb5d0a5cc5
Matthew Lickei <matthew@rentping.com>
2017-03-08 17:53:01 -0600

    Fixed control bar bugs. Added inside outside button position options w/out styling.

--- src/comparison-viewer-src.js
+++ src/comparison-viewer-src.js
@@ -12,11 +12,10 @@
 			showRightButtonLabel: 'Show Right Side',
 			showRightButton: false,
 			revertButtonText: 'Revert',
-			showRevertButton: false
+			showRevertButton: false,
+			buttonPlacement: 'outside' //Opts outside, inside
 			//TODO add options for the slider handle html
 			//TODO add events/functions
-			//TODO toggle revert button
-			//TODO toggle ability to revert back to hover effect
 		}, options);
 
 		return this.each(function() {
@@ -103,35 +102,32 @@
 	}
 
 	function updateHeight(viewer) {
-		viewer.$viewer.css('min-height', viewer.$innerViews.children().outerHeight());
-	}
-
-	function checkForWidthUpdate() {
-
-	}
-
-	function checkForHeightUpdate() {
-
-	}
-
-	function checkForWidthHeightUpdates() {
-		//TODO Recursion
+		viewer.$viewOverflowWrap.css('min-height', viewer.$innerViews.children().outerHeight());
 	}
 
 	function revert(viewer) {
-
+		viewer.$viewer.removeClass('position-set no-hover-effect');
+		viewer.$leftView.css('right', '');
+		viewer.$rightView.css('left', '');
 	}
 
-	function makeGeneralActionsContainer(viewer, $target) {
-		return $('<div class="comparison-viewer-actions"></div>').appendTo($target);
+	function makeGeneralActionsContainer(viewer, $target, useAppend) {
+		if(useAppend) {
+			return $('<div class="comparison-viewer-actions"></div>').appendTo($target);
+		} else {
+			return $('<div class="comparison-viewer-actions"></div>').prependTo($target);
+		}
 	}
 
 	function setupButtons(viewer) {
+		viewer.$viewer.addClass(viewer.options.buttonPlacement + '-btn-placement');
+
 		var $generalActionsContainer;
 
 		if(viewer.options.showRevertButton) {
 			if(typeof $generalActionsContainer === 'undefined' || !$generalActionsContainer.length) {
-				$generalActionsContainer = makeGeneralActionsContainer(viewer, viewer.$viewer);
+				$generalActionsContainer = makeGeneralActionsContainer(viewer, viewer.$viewer, false)
+					.addClass(viewer.options.buttonPlacement + '-main-actions main-btn-actions');
 			}
 
 			$('<button class="comparison-viewer-btn revert-btn">' + viewer.options.revertButtonText + '</button>')
@@ -141,36 +137,45 @@
 				});
 		}
 
-		if(viewer.options.showLeftButton) {
-			$('<button class="comparison-viewer-btn show-left-btn">' + viewer.options.showLeftButtonLabel + '</button>')
-				.appendTo(makeGeneralActionsContainer(viewer, viewer.$leftView))
-				.on('click', function () {
-					if (!viewer.$viewer.hasClass('position-set')) {
-						viewer.$viewer.addClass('position-set');
-					}
 
+		var leftRightActionsContainer = makeGeneralActionsContainer(viewer, viewer.$viewer, true)
+			.addClass('left-right-actions');
+
+		if(viewer.options.showLeftButton) {
+			var $bar = $('<button class="comparison-viewer-btn show-left-btn">' + viewer.options.showLeftButtonLabel + '</button>')
+				.on('click', function () {
+					viewer.$viewer.addClass('no-hover-effect').removeClass('position-set');
 					viewer.$leftView.css('right', 0);
 					viewer.$rightView.css('left', 0);
 				});
+
+			if(viewer.options.buttonPlacement == 'outside') {
+				$bar.appendTo(leftRightActionsContainer);
+			} else if(viewer.options.buttonPlacement == 'inside') {
+				$bar.appendTo(makeGeneralActionsContainer(viewer, viewer.$leftView, true));
+			}
 		}
 
 		if(viewer.options.showRightButton) {
-			$('<button class="comparison-viewer-btn show-right-btn">' + viewer.options.showRightButtonLabel + '</button>')
-				.appendTo(makeGeneralActionsContainer(viewer, viewer.$rightView))
+			var $bar = $('<button class="comparison-viewer-btn show-right-btn">' + viewer.options.showRightButtonLabel + '</button>')
 				.on('click', function () {
-					if (!viewer.$viewer.hasClass('position-set')) {
-						viewer.$viewer.addClass('position-set');
-					}
-
+					viewer.$viewer.addClass('no-hover-effect').removeClass('position-set');
 					viewer.$leftView.css('right', '100%');
 					viewer.$rightView.css('left', 0);
 				});
+
+			if(viewer.options.buttonPlacement == 'outside') {
+				$bar.appendTo(leftRightActionsContainer);
+			} else if(viewer.options.buttonPlacement == 'inside') {
+				$bar.appendTo(makeGeneralActionsContainer(viewer, viewer.$rightView, true));
+			}
 		}
 	}
 
 	ComparisonViewer.prototype.init = function() {
 		var viewer = this;
-		viewer.$viewer = $('<div class="comparison-viewer"><div class="left-view view"><div class="view-ctrl-bar"><div class="ctrl-circle"></div></div><div class="inner-view-wrapper"><div class="inner-view">' + this.leftHtml + '</div></div></div><div class="right-view view"><div class="inner-view-wrapper"><div class="inner-view">' + this.rightHtml + '</div></div></div></div>').appendTo(viewer.$container);
+		viewer.$viewer = $('<div class="comparison-viewer"><div class="comparison-viewer-overflow-wrap"><div class="left-view view"><div class="view-ctrl-bar"><div class="ctrl-circle"></div></div><div class="inner-view-wrapper"><div class="inner-view">' + this.leftHtml + '</div></div></div><div class="right-view view"><div class="inner-view-wrapper"><div class="inner-view">' + this.rightHtml + '</div></div></div></div></div>').appendTo(viewer.$container);
+		viewer.$viewOverflowWrap = this.$viewer.find('.comparison-viewer-overflow-wrap');
 		viewer.$innerViews = this.$viewer.find('.inner-view');
 		viewer.$leftView = this.$viewer.find('.left-view');
 		viewer.$rightView = this.$viewer.find('.right-view');
@@ -191,8 +196,10 @@
 		function buildDraggable() {
 			viewer.draggable = viewer.$controlBar.draggable({
 				axis: "x",
-				//FIXME use containment array [(X1 + barWidth), Y1, (X2 - barWidth), Y2] see http://stackoverflow.com/questions/14860845/jquery-draggable-containment-using-array-is-not-working-as-expected
 				containment: viewer.$viewer.get(0),
+				cursorAt: {
+					left: 36
+				},
 				drag: function (evt, ui) {
 					viewer.updateViewByDrag(ui.position.left);
 				},
@@ -217,10 +224,13 @@
 			buildDraggable();
 		}
 
-		viewer.updateInnerViewsWidth();
-		updateHeight(viewer);
-		viewer.updateInnerViewsWidth();
-		updateHeight(viewer);
+		//This could probably go on for an infinite amount of time, but let's not
+		setTimeout(function () {
+			viewer.updateInnerViewsWidth();
+			updateHeight(viewer);
+			viewer.updateInnerViewsWidth();
+			updateHeight(viewer);
+		}, 250);
 
 		viewer.inited = true;
 	};
